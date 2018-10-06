f080b48330bbb738b3bcf4479efa4ec1ec941e5d
bloodyavagard@mail.ru <bloodyavangard@mail.ru>
2018-10-06 15:39:10 +0300

    HomeWork 1 done

--- Task1_1.hs
+++ Task1_1.hs
@@ -2,26 +2,51 @@ module Task1_1 where
 
 import Todo(todo)
 
+data BinOp = Plus | Minus | Mult 
+  deriving (Show, Eq)
+
 data Term = IntConstant{ intValue :: Int }           -- числовая константа
             | Variable{ varName :: String }          -- переменная
-            | BinaryTerm{ lhv :: Term, rhv :: Term } -- бинарная операция
+            | BinaryTerm{ lhv :: Term, binOp :: BinOp, rhv :: Term } -- бинарная операция
             deriving(Show,Eq)
 
 -- Для бинарных операций необходима не только реализация, но и адекватные
 -- ассоциативность и приоритет
 (|+|) :: Term -> Term -> Term
-(|+|) l r = todo
+(|+|) l r = BinaryTerm l Plus r
 (|-|) :: Term -> Term -> Term
-(|-|) l r = todo
+(|-|) l r = BinaryTerm l Minus r
 (|*|) :: Term -> Term -> Term
-(|*|) l r = todo
+(|*|) l r = BinaryTerm l Mult r
+
+infixl 6 |+| 
+infixl 6 |-| 
+infixl 7 |*| 
 
 -- Заменить переменную `varName` на `replacement`
 -- во всём выражении `expression`
 replaceVar :: String -> Term -> Term -> Term
-replaceVar varName replacement expression = todo
+replaceVar varName replacement expression = case expression of
+                                              v@(Variable s)    -> if (s == varName) then replacement else v
+                                              BinaryTerm l op r -> BinaryTerm (replaceVar varName replacement l) op (replaceVar varName replacement r)
+                                              _                 -> expression  
 
 -- Посчитать значение выражения `Term`
 -- если оно состоит только из констант
 evaluate :: Term -> Term
-evaluate expression = todo
+evaluate (BinaryTerm l op r) = evaluate' (evaluate l) op (evaluate r)
+   where 
+    evaluate' :: Term -> BinOp -> Term -> Term
+    evaluate' (IntConstant c1) Plus  (IntConstant c2) = IntConstant $ c1 + c2
+    evaluate' (IntConstant 0)  Plus  t                = t
+    evaluate' t                Plus  (IntConstant 0)  = t                
+    evaluate' (IntConstant c1) Minus (IntConstant c2) = IntConstant $ c1 - c2
+    evaluate' t                Minus (IntConstant 0)  = t
+    evaluate' (IntConstant c1) Mult  (IntConstant c2) = IntConstant $ c1 * c2
+    evaluate' (IntConstant 0)  Mult  _                = IntConstant 0  
+    evaluate' (IntConstant 1)  Mult  t                = t
+    evaluate' _                Mult  (IntConstant 0)  = IntConstant 0  
+    evaluate' t                Mult  (IntConstant 1)  = t                 
+    evaluate' l                op    r                = BinaryTerm  l op r 
+
+evaluate term = term
--- Task1_2.hs
+++ Task1_2.hs
@@ -1,7 +1,7 @@
 module Task1_2 where
 
 import Todo(todo)
-
+import Prelude hiding (gcd, pow)
 -- синус числа (формула Тейлора)
 sin :: Double -> Double
 sin x = todo
@@ -12,25 +12,55 @@ cos x = todo
 
 -- наибольший общий делитель двух чисел
 gcd :: Integer -> Integer -> Integer
-gcd x y = todo
+gcd x y | x <= 0 || y <= 0 = error "Unsupported: negative numbers."
+        | x == 0 && y == 0 = error "Error: x = 0; y = 0."
+        | y == 0 = x
+        | otherwise = gcd y (x `rem` y)
 
 -- существует ли полный целочисленный квадрат в диапазоне [from, to)?
 doesSquareBetweenExist :: Integer -> Integer -> Bool
-doesSquareBetweenExist from to = todo
+doesSquareBetweenExist from to = let 
+                                   flSqrt = floor . sqrt . fromIntegral
+                                   ceSqrt = ceiling . sqrt . fromIntegral
+                                 in (flSqrt (to - 1) - ceSqrt from) >= 0 
+
 
 -- является ли дата корректной с учётом количества дней в месяце и
 -- вискокосных годов?
 isDateCorrect :: Integer -> Integer -> Integer -> Bool
-isDateCorrect day month year = todo
+isDateCorrect day month year | day <= 0 || month <= 0 || year <= 0 = False
+                             | not (month `elem` monthes) = False  
+                             | day `elem` [1 .. days !! (fromIntegral month - 1)] = True
+                             | isLeapFebruary year month && day == 29 = True
+                             | otherwise = False
+  where
+    isLeapYear year = (year `rem` 4 == 0 && year `rem` 100 /= 0) || (year `rem` 400 == 0)
+    isLeapFebruary year month = month == 2 && isLeapYear year
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    monthes = [1..12]
 
 -- возведение числа в степень, duh
 -- готовые функции и плавающую арифметику использовать нельзя
 pow :: Integer -> Integer -> Integer
-pow x y = todo
+pow x 0 = 1
+pow x y | y < 0 = error "Negative pow"
+        | otherwise = pow' x y x
+  where
+    pow' x 1 _ = x
+    pow' x y z = pow' (x * z) (y - 1) z  
 
 -- является ли данное число простым?
 isPrime :: Integer -> Bool
-isPrime x = todo
+isPrime x | x <= 0 = error "Number is <= 0"
+          | otherwise = null [y | y <- [2..x-1], x `rem` y == 0]
+
+-- a bit more effective implementation
+isPrime' :: Integer -> Bool
+isPrime' x | x <= 0 = error "Number is <= 0"
+           | otherwise = checkPrime x [2 .. x-1]
+  where
+    checkPrime x [] = True
+    checkPrime x (y:ys) = if (x `rem` y == 0) then False else checkPrime x ys
 
 type Point2D = (Double, Double)
 
