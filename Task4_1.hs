1bf35c5a8b891588cc63df071abbd4cf3f4827d4
AndreiChugunov <bloodyavangard@mail.ru>
2018-11-19 14:02:38 +0300

    Task4_1, Task4_2 are done!

--- Task4_1.hs
+++ Task4_1.hs
@@ -1,7 +1,18 @@
 module Task4_1 where
-
+import Control.Monad (ap, liftM)
 -- Монада над функцией. В качестве входного значения `fun` может быть что угодно
 -- Собственно, почему бы не `String`?
 data FunMonad a = FunMonad { fun :: String -> a }
 
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FunMonad`
+
+instance Functor FunMonad where
+  fmap = liftM
+
+instance Applicative FunMonad where
+  pure = return
+  (<*>) = ap
+
+instance Monad FunMonad where
+  return x = FunMonad (\_ -> x)
+  x >>= f = FunMonad (\str -> (fun $ f $ (fun x) str) str)
--- Task4_2.hs
+++ Task4_2.hs
@@ -1,7 +1,23 @@
 module Task4_2 where
+import Control.Monad (ap, liftM)
 
 data FourOf a = FourOf a a a a deriving(Show,Eq)
 
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FourOf`
--- таким образом, что 
+-- таким образом, что
 -- do { x <- FourOf 1 2 3 4; y <- FourOf 4 6 7 8; return $ x + y } === FourOf 5 8 10 12
+
+instance Functor FourOf where
+  fmap = liftM
+
+instance Applicative FourOf where
+  pure = return
+  (<*>) = ap
+
+instance Monad FourOf where
+  return x = FourOf x x x x
+  (FourOf a b c d) >>= f = let FourOf a' _ _ _ = f a
+                               FourOf _ b' _ _ = f b
+                               FourOf _ _ c' _ = f c
+                               FourOf _ _ _ d' = f d
+                           in FourOf a' b' c' d'
